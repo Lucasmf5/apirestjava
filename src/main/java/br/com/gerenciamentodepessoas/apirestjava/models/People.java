ebcc358b0b5a2d85d8e194ee4d3a5d66f06d8e21
lucasmf5 <lucas.mendesf1@gmail.com>
2024-05-01 18:11:00 -0300

    Projeto finalizado

--- src/main/java/br/com/gerenciamentodepessoas/apirestjava/models/People.java
+++ src/main/java/br/com/gerenciamentodepessoas/apirestjava/models/People.java
@@ -1,54 +1,80 @@
 package br.com.gerenciamentodepessoas.apirestjava.models;
 
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.NumberFormat;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 public class People {
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+    private Integer id;
 
+    @Getter
+    @Setter
     @Column(nullable = false)
+    @NotBlank(message = "O nome deve ser informado.")
     private String name;
 
+    @Getter
+    @Setter
     @Column(nullable = false)
+    @Min(19000000)
     private Integer bornDate;
 
+    @Getter
+    @Setter
     @Column(nullable = false)
+    @NotBlank(message = "O endereço principal deve ser informado.")
     private String principalAdress;
 
+    @Getter
+    @Setter
     @Column()
-    private String adress;
+    private String secondAdress;
 
+    @Getter
+    @Setter
     @Column(nullable = false)
-    private String publicPlace;
+    @NotBlank(message = "O logradouro do endereço principal deve ser informado.")
+    private String publicPlacePrincipalAdress;
 
+    @Getter
+    @Setter
     @Column(nullable = false)
-    private Integer cep;
+    @Min(value = 10000000, message = "O CEP deve conter oito digitos")
+    @Max(value = 100000000, message = "O CEP deve conter oito digitos")
+    private Integer cepPrincipalAdress;
 
+    @Getter
+    @Setter
     @Column(nullable = false)
-    private Integer number;
+    @Min(value = 0)
+    private Integer numberPrincipalAdress;
 
+    @Getter
+    @Setter
     @Column(nullable = false)
-    private Integer city;
+    @NotBlank(message = "A cidade do endereço principal deve ser informada.")
+    private String cityPrincipalAdress;
 
+    @Getter
+    @Setter
     @Column(nullable = false)
-    private String state;
-
-    public People(String name, Integer bornDate, String adress, String publicPlace, Integer cep, Integer number,
-            Integer city, String state) {
-        this.name = name;
-        this.bornDate = bornDate;
-        this.adress = adress;
-        this.publicPlace = publicPlace;
-        this.cep = cep;
-        this.number = number;
-        this.city = city;
-        this.state = state;
-    }
+    @NotBlank(message = "O estado do endereço principal deve ser informado.")
+    private String statePrincipalAdress;
 
 }
